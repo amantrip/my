97e63ab5acf7487ad2ace263ab8cd0a940123dc9
Akhil Mantripragada <akhimantripragada@gmail.com>
2018-09-16 17:57:07 -0400

    Updated website to point to my resume

---
+++ app/views/newindex.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Akhilesh Mantripragada</title>
+
+    <meta name="description" content="Akhilesh Mantripragada. Software Engineer and Web Specialist." />
+    <meta name="keywords" content="Akhilesh Mantripagada Edulinc" />
+    <meta name="author" content="Akhilesh Mantripragada" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+    <style>
+        .pdfobject-container { height: 750px;}
+        .pdfobject { border: 1px solid #eeeeee; }
+    </style>
+</head>
+<body>
+<div id="example1"></div>
+<!-- pdf object -->
+<script src="/js/pdfobject.js" type="text/javascript" charset="utf-8"></script>
+<script>PDFObject.embed("/docs/Resume-Akhilesh-Mantripragada.pdf", "#example1");</script>
+</body>
+</html>
